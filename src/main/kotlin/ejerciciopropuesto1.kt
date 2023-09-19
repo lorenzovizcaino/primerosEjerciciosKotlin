9205b8467cb65b1de38c50d5fc2a925e04fd5062
Antonio Lorenzo <lorenzovizcaino@gmail.com>
2023-09-19 13:06:17 +0200

    190923_13:06

---
+++ src/main/kotlin/ejerciciopropuesto1.kt
@@ -0,0 +1,18 @@
+
+
+
+/*
+Definir una variable inmutable con el valor 50 que representa el lado de un cuadrado,
+en otras dos variables inmutables almacenar la superficie y el perímetro del cuadrado.
+Mostrar la superficie y el perímetro por la Consola.
+
+ */
+
+
+fun main() {
+    val lado=50
+    val superpicie=lado*lado
+    val perimetro=lado*4
+    println("Un cuadrado de lado $lado tiene:\nSuperficie de: $superpicie\nPerimetro de: $perimetro")
+
+}
--- src/main/kotlin/primerosPasos.kt
+++ src/main/kotlin/primerosPasos.kt
@@ -1,10 +1,8 @@
-fun main(parametro: Array<String>) {
-    val valor1: Int
-    val valor2: Int
-    valor1 = 100
-    valor2 = 400
-    var resultado: Int
-    resultado = valor1 + valor2
+fun main() {
+    val valor1: Int=100
+    val valor2: Int=400
+
+    var resultado= valor1 + valor2
     println("La suma de $valor1 + $valor2 es $resultado")
     resultado = valor1 * valor2
     println("El producto de $valor1 * $valor2 es $resultado")
